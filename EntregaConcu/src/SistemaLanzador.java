90a7e4502c898a495f91d4d7c1f30f5889423702
ClaraGarciaa <123200581+ClaraGarciaBartolome@users.noreply.github.com>
2023-05-28 03:40:57 +0200

    Update SistemaLanzador.java

--- EntregaConcu/src/SistemaLanzador.java
+++ EntregaConcu/src/SistemaLanzador.java
@@ -1,6 +1,23 @@
 public class SistemaLanzador {
     
 	public static void main(String[] args) {
-        System.out.println("	SIMULACIÓN VIDEOJUEGO	");
-	}    
+        int numTiposEnemigos = 4;
+        int maxEnemigos = 20;
+
+        //Inicializamos juego
+        Juego juego = new Juego(numTiposEnemigos, maxEnemigos);
+        
+        System.out.println("	SIMULACIÃ“N VIDEOJUEGO	");
+        
+        for (int i = 0; i < numTiposEnemigos; i++) {
+
+            Thread hiloEnemigo = new Thread(new ActividadEnemiga(i, juego));
+            Thread hiloAliado = new Thread(new ActividadAliada(i, juego));
+
+            hiloEnemigo.start();
+            hiloAliado.start();
+
+        }
+    }
 }
+
